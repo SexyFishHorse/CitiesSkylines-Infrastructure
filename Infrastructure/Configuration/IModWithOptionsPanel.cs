15773623842202e5e72c4fad328e03f4395f8961
Asbjørn Hansen <ah@fenris.io>
2016-06-26 18:03:37 +0200

    created interface for remembering the options panel method signature

---
+++ Infrastructure/Configuration/IModWithOptionsPanel.cs
@@ -0,0 +1,12 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Configuration
+{
+    using ICities;
+    using JetBrains.Annotations;
+
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public interface IModWithOptionsPanel
+    {
+        // ReSharper disable once InconsistentNaming
+        void OnSettingsUI(UIHelperBase uiHelper);
+    }
+}
