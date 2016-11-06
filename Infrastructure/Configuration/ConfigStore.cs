5857eea0b28804efa989dd884a83a6326b0a8c9b
Asbjørn Hansen <ah@fenris.io>
2016-11-06 14:00:19 +0100

    added config store

---
+++ Infrastructure/Configuration/ConfigStore.cs
@@ -0,0 +1,118 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+    using IO;
+    using JetBrains.Annotations;
+    using Validation.Arguments;
+
+    public class ConfigStore : IConfigStore
+    {
+        public const string ConfigFileName = "ModConfiguration.xml";
+
+        private XmlSerializer serializer;
+
+        public ConfigStore(string modFolderName)
+        {
+            var modFolderPath = GamePaths.GetModFolderPath(GameFolder.Mods);
+            modFolderPath = Path.Combine(modFolderPath, modFolderName);
+
+            Directory.CreateDirectory(modFolderPath);
+
+            ConfigFileInfo = new FileInfo(Path.Combine(modFolderPath, ConfigFileName));
+
+            serializer = new XmlSerializer(typeof(ModConfiguration));
+
+            if (!ConfigFileInfo.Exists)
+            {
+                SaveConfigToFile(new ModConfiguration());
+            }
+        }
+
+        public FileInfo ConfigFileInfo { get; private set; }
+
+        public T GetSetting<T>(string key)
+        {
+            key.ShouldNotBeNull("key");
+
+            var modConfiguration = LoadConfigFromFile();
+
+            if (modConfiguration.Settings.Any(x => x.Key == key))
+            {
+                return (T) modConfiguration.Settings.Single(x => x.Key == key).Value;
+            }
+
+            return default(T);
+        }
+
+        public bool HasSetting(string key)
+        {
+            var config = LoadConfigFromFile();
+
+            return config.Settings.Any(x => x.Key == key);
+        }
+
+        public void RemoveSetting(string key)
+        {
+            key.ShouldNotBeNull("key");
+
+            var config = LoadConfigFromFile();
+
+            config.Settings.Remove(config.Settings.Find(x => x.Key == key));
+
+            SaveConfigToFile(config);
+        }
+
+        public void SaveSetting<T>(string key, T value)
+        {
+            key.ShouldNotBeNull("key");
+
+            if (value == null)
+            {
+                var message =
+                    string.Format(
+                        "The configuration value for the key {0} is null. Use RemoveSetting to remove a value",
+                        key);
+
+                throw new ArgumentNullException("value", message);
+            }
+
+            var modConfiguration = LoadConfigFromFile();
+
+            if (modConfiguration.Settings.Any(x => x.Key == key))
+            {
+                modConfiguration.Settings.Remove(modConfiguration.Settings.Single(x => x.Key == key));
+            }
+
+            modConfiguration.Settings.Add(new KeyValuePair<string, object>(key, value));
+
+            SaveConfigToFile(modConfiguration);
+        }
+
+        private ModConfiguration LoadConfigFromFile()
+        {
+            if (!ConfigFileInfo.Exists)
+            {
+                return new ModConfiguration();
+            }
+
+            using (var fileStream = ConfigFileInfo.OpenRead())
+            {
+                var config = serializer.Deserialize(fileStream) as ModConfiguration;
+
+                return config ?? new ModConfiguration();
+            }
+        }
+
+        private void SaveConfigToFile([NotNull] ModConfiguration modConfiguration)
+        {
+            using (var fileStream = ConfigFileInfo.Open(FileMode.Create, FileAccess.Write))
+            {
+                serializer.Serialize(fileStream, modConfiguration);
+            }
+        }
+    }
+}
---
+++ Infrastructure/Configuration/IConfigStore.cs
@@ -0,0 +1,21 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Configuration
+{
+    using System.IO;
+    using JetBrains.Annotations;
+
+    [UsedImplicitly(ImplicitUseTargetFlags.Members)]
+    public interface IConfigStore
+    {
+        [NotNull]
+        FileInfo ConfigFileInfo { get; }
+
+        [CanBeNull]
+        T GetSetting<T>(string key);
+
+        void RemoveSetting(string key);
+
+        void SaveSetting<T>(string key, T value);
+
+        bool HasSetting(string key);
+    }
+}
