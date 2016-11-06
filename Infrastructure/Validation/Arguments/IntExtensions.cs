079b98d63c03899a0c9a7ed5dc71bad0dbee4ea0
Asbjørn Hansen <ah@fenris.io>
2016-11-06 13:58:28 +0100

    added argument validation classes

---
+++ Infrastructure/Validation/Arguments/IntExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Validation.Arguments
+{
+    using System;
+    using JetBrains.Annotations;
+
+    public static class IntExtensions
+    {
+        [AssertionMethod]
+        public static void ShouldBeGreaterThanZero(
+            this int value,
+            [InvokerParameterName] [NotNull] string parameterName)
+        {
+            if (!(value > 0))
+            {
+                throw new ArgumentOutOfRangeException(parameterName, value, "The value must be greater than zero");
+            }
+        }
+    }
+}
