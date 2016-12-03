11d8ec6d490906a2e5663033a30bd6b9fd67c592
Asbjørn Hansen <ah@fenris.io>
2016-12-03 04:27:59 +0100

    added base class for mods

---
+++ Infrastructure/UserModBase.cs
@@ -0,0 +1,25 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure
+{
+    using System.Diagnostics.CodeAnalysis;
+    using ICities;
+    using UI;
+
+    public abstract class UserModBase : IUserMod
+    {
+        public abstract string Description { get; }
+
+        public abstract string Name { get; }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public void OnSettingsUI(UIHelperBase uiHelperBase)
+        {
+            var uiHelper = uiHelperBase.AsStronglyTyped();
+
+            ConfigureOptionsUi(uiHelper);
+        }
+
+        protected virtual void ConfigureOptionsUi(IStronglyTypedUiHelper uiHelper)
+        {
+        }
+    }
+}
