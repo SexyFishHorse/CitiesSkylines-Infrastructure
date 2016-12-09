aae745e2218d01a84f5da40164e6de88e62638ec
Asbjørn Hansen <ah@fenris.io>
2016-12-09 01:04:33 +0100

    added various extensions

---
+++ Infrastructure/Extensions/LoadModeExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Extensions
+{
+    using System.Collections.Generic;
+    using ICities;
+
+    public static class LoadModeExtensions
+    {
+        private static readonly ICollection<SimulationManager.UpdateMode> GameModes = new HashSet<SimulationManager.UpdateMode>
+        {
+            SimulationManager.UpdateMode.LoadGame,
+            SimulationManager.UpdateMode.NewGameFromMap,
+            SimulationManager.UpdateMode.NewGameFromScenario,
+            SimulationManager.UpdateMode.LoadScenario
+        };
+
+        public static bool IsGameOrScenario(this LoadMode mode)
+        {
+            var updateMode = (SimulationManager.UpdateMode)mode;
+
+            return GameModes.Contains(updateMode);
+        }
+
+        public static bool IsScenario(this LoadMode mode)
+        {
+            var updateMode = (SimulationManager.UpdateMode)mode;
+
+            return (updateMode == SimulationManager.UpdateMode.NewGameFromScenario) ||
+                   (updateMode == SimulationManager.UpdateMode.LoadScenario);
+        }
+    }
+}
---
+++ Infrastructure/UI/Extensions/UISliderExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.UI.Extensions
+{
+    using ColossalFramework.UI;
+    using Validation.Arguments;
+
+    public static class UISliderExtensions
+    {
+        public static string GetLabelText(this UISlider slider)
+        {
+            var panel = slider.GetComponentInParent<UIPanel>();
+            var uiLabel = panel.Find<UILabel>("Label");
+
+            return uiLabel.text;
+        }
+
+        public static void SetLabelText(this UISlider slider, string label)
+        {
+            label.ShouldNotBeNullOrEmpty("label");
+
+            var panel = slider.GetComponentInParent<UIPanel>();
+            var uiLabel = panel.Find<UILabel>("Label");
+
+            uiLabel.text = label;
+        }
+    }
+}
