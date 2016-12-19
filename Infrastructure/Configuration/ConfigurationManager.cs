864fbae56b842bbe2f2f06a68b149aa8d6f7e605
Asbjørn Hansen <ash@moviestarplanet.com>
2016-12-19 10:57:53 +0100

    added optional logging to configuration manager

--- Infrastructure/Configuration/ConfigurationManager.cs
+++ Infrastructure/Configuration/ConfigurationManager.cs
@@ -1,7 +1,10 @@
 ﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Configuration
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using JetBrains.Annotations;
+    using SexyFishHorse.CitiesSkylines.Logger;
 
     public class ConfigurationManager
     {
@@ -14,39 +17,36 @@
             configStore = new ConfigStore(modName, modName + ".xml");
         }
 
+        public ILogger Logger { get; set; }
+
         public T GetSetting<T>(string settingKey)
         {
             EnsureConfigLoaded();
 
             if (configuration.Settings.Any(x => x.Key == settingKey))
             {
-                return (T)configuration.Settings.Single(x => x.Key == settingKey).Value;
+                var value = configuration.Settings.Single(x => x.Key == settingKey).Value;
+                try
+                {
+                    return (T)value;
+                }
+                catch (InvalidCastException ex)
+                {
+                    var message = string.Format(
+                        "Tried to cast value '{0}' of type '{1}' to '{2}'.",
+                        value,
+                        value.GetType().Name,
+                        typeof(T).Name);
+
+                    TryLogError(message);
+
+                    throw new InvalidCastException(message, ex);
+                }
             }
+
+            TryLog("No setting found for {0}", settingKey);
 
             return default(T);
-        }
-
-        public void SaveSetting(string settingKey, object value)
-        {
-            EnsureConfigLoaded();
-
-            if (configuration.Settings.Any(x => x.Key == settingKey))
-            {
-                configuration.Settings.Remove(configuration.Settings.Single(x => x.Key == settingKey));
-            }
-
-            var newKeyValuePair = new KeyValuePair<string, object>(settingKey, value);
-            configuration.Settings.Add(newKeyValuePair);
-
-            configStore.SaveConfigToFile(configuration);
-        }
-
-        private void EnsureConfigLoaded()
-        {
-            if (configuration == null)
-            {
-                configuration = configStore.LoadConfigFromFile();
-            }
         }
 
         public void Migrate<T>(string oldSettingKey, string newSettingKey)
@@ -64,6 +64,35 @@
             SaveSetting(newSettingKey, (T)settingValue.Value);
 
             configStore.SaveConfigToFile(configuration);
+
+            TryLog("Migrated {0} to {1}", oldSettingKey, newSettingKey);
+        }
+
+        public void SaveSetting(string settingKey, object value)
+        {
+            EnsureConfigLoaded();
+
+            if (configuration.Settings.Any(x => x.Key == settingKey))
+            {
+                configuration.Settings.Remove(configuration.Settings.Single(x => x.Key == settingKey));
+            }
+
+            var newKeyValuePair = new KeyValuePair<string, object>(settingKey, value);
+            configuration.Settings.Add(newKeyValuePair);
+
+            configStore.SaveConfigToFile(configuration);
+
+            TryLog("Saved setting " + settingKey + " with value " + value);
+        }
+
+        private void EnsureConfigLoaded()
+        {
+            if (configuration == null)
+            {
+                configuration = configStore.LoadConfigFromFile();
+
+                TryLog("Loaded config from file");
+            }
         }
 
         private void RemoveSetting(string settingKey)
@@ -72,6 +101,25 @@
             configuration.Settings.Remove(setting);
 
             configStore.SaveConfigToFile(configuration);
+
+            TryLog("Removed setting for {0}", settingKey);
+        }
+
+        [StringFormatMethod("format")]
+        private void TryLog(string format, params object[] args)
+        {
+            if (Logger != null)
+            {
+                Logger.Info(format, args);
+            }
+        }
+
+        private void TryLogError(string message)
+        {
+            if (Logger != null)
+            {
+                Logger.Error(message);
+            }
         }
     }
 }
