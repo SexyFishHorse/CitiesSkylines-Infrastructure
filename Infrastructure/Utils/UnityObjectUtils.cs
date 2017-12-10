dd71abd202a6713177b835d201c4a1a62bd9ff44
Asbjørn Hansen <ah@fenris.io>
2017-12-10 16:27:13 +0100

    add helper method for finding a specific object

---
+++ Infrastructure/Utils/UnityObjectUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Utils
+{
+    using System.Linq;
+    using JetBrains.Annotations;
+    using SexyFishHorse.CitiesSkylines.Infrastructure.Validation.Arguments;
+    using UnityObject = UnityEngine.Object;
+
+    public static class UnityObjectUtils
+    {
+        [PublicAPI]
+        [CanBeNull]
+        public static T FindObject<T>([NotNull] string name) where T : UnityObject
+        {
+            name.ShouldNotBeNullOrEmpty("name");
+
+            return UnityObject.FindObjectsOfType<T>().FirstOrDefault(x => x.name == name);
+        }
+    }
+}
